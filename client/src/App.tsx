b2a397d084ce5249c24c5f14a993f8726ea2262d
TamasSallai <tamas.sallai.hu@gmail.com>
2023-06-13 16:09:55 +0200

    Created first request handler

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,31 @@
+import { useState } from 'react'
 import './App.css'
 
+type ResultData = {
+  result: string
+}
+
 const App = () => {
-  return <div>Hello World</div>
+  const [result, setResult] = useState<string>('')
+
+  const handleFetchResult = async () => {
+    const response = await fetch('api/result')
+    const data = (await response.json()) as ResultData
+    //The response should be guarded instead of type assertion
+    if (data.result) setResult(data.result)
+  }
+
+  return (
+    <div>
+      <input
+        type="number"
+        value={result}
+        onChange={(e) => setResult(e.target.value)}
+      />
+      <button>Save result</button>
+      <button onClick={handleFetchResult}>Get last result</button>
+    </div>
+  )
 }
 
 export default App
