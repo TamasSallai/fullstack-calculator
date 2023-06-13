432b18eea6de7bc5d10ae5d698e98449cb15bafd
TamasSallai <tamas.sallai.hu@gmail.com>
2023-06-13 18:53:24 +0200

    created post endpoint for saving result

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import fs from 'fs'
+
 const app = express()
+app.use(express.json())
 
 app.get('/api', (req, res) => {
   res.send('Hello World')
@@ -10,11 +12,22 @@ app.get('/api/result', (req, res) => {
   fs.readFile('./data/result.json', (err, data) => {
     if (err) {
       if (err.code === 'ENOENT') {
-        return res.status(404).send({ error: 'No such file or directory :(' })
+        return res.status(404).send({ error: 'No such file or directory. :(' })
       }
-      return res.status(500).send({ error: 'Internal Server error :(' })
+      return res.status(500).send({ error: 'Internal server error. :(' })
     }
     return res.status(200).send(JSON.parse(data.toString()))
+  })
+})
+
+app.post('/api/result', (req, res) => {
+  const data = JSON.stringify(req.body)
+  fs.writeFile('./data/result.json', data, (err) => {
+    if (err) {
+      console.log(err)
+      return res.status(500).send({ error: 'Internal server error. :(' })
+    }
+    return res.status(200).send({ message: 'Result successfuly saved. :)' })
   })
 })
 
