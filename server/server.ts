e86d16a98cab9e305e20a95515f40503f7000be9
TamasSallai <tamas.sallai.hu@gmail.com>
2023-06-13 16:01:41 +0200

    reacted the first endpoint for reading a json file

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,21 @@
 import express from 'express'
+import fs from 'fs'
 const app = express()
 
 app.get('/api', (req, res) => {
   res.send('Hello World')
+})
+
+app.get('/api/result', (req, res) => {
+  fs.readFile('./data/result.json', (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        return res.status(404).send({ error: 'No such file or directory :(' })
+      }
+      return res.status(500).send({ error: 'Internal Server error :(' })
+    }
+    return res.status(200).send(JSON.parse(data.toString()))
+  })
 })
 
 const PORT = 8000
